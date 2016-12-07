a58798575c7b11f86b04e9bac21dfba65504440d
Chau Huynh <cmhuynh@gmail.com>
2016-12-07 11:59:58 +0700

    ex1: complete required assignments

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,8 +13,19 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+% from the lecture, our hypothesis / prediction function is
+% product of theta multiples with X
+%
+% X is a matrix mx2, with padding of 1 and one feature, where m is size of data set
+% since theta is presented as a vector 2xm
+% to multiple theta to X, we need to transpose both theta and X, to the dimension of 1x2 and 2xm
+% and transpose the result to verctor mx1
+% 
+% a shorthand is to do X * theta, or (mx2) * (2x1), to produce the result vector	
+	
+predictions = X * theta;
+sqrErrors = (predictions - y) .^ 2;
+J = 1/(2 * m) * sum(sqrErrors);
 
 
 % =========================================================================
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -19,8 +19,25 @@ for iter = 1:num_iters
 
 
 
-
-
+	% inspired by te Vectorization lecture
+	% and try to apply into each calculation of each theta 0, 1, ...
+	%
+	% for each theta (0), first to calucate the prediction - y, which is a vector of mx1, m is size of data set
+	% then for each value of the result vector, multiply with the vector X(:,0) of size mx1
+	% index 0 of X is corresponding to theta (0), similar to 1, 2, ... n
+	% then sum it up across the data set
+	%
+	% verctor error mx1 and X mx2, and similar to the job in computeCost, its a product of transpose (X) * error, or 2xm * mx1, result in 2*1
+	% of which average of, multiply with learning rate alpha, is the delta for each iteration of theta
+	% sanity check with octave
+	% say vector error is [1; 2; 3] and X is [1, 1; 1, 2; 1, 3]
+	% to calculate for theta 0, pick column 0 of X on all data set (size 3 here), so its 1*1 + 2*1 + 3*1
+	% and similarly for theta 1, its 1*1 + 2*2 + 3*3
+	% vectorization: transpose(X) * error
+		
+	error = (X * theta) - y;
+	delta = X' * error;
+	theta = theta - (alpha/m) * delta;
 
 
     % ============================================================
